f9893f8dbd280809ccf4263b1db39281dc924f2e
Manuel Vinent Guilarte <mjavint@gmail.com>
2022-06-03 17:07:33 -0400

    [UPG] Corregido error que impedia crear un empleado

--- l10n_cl_hr/model/hr_employee.py
+++ l10n_cl_hr/model/hr_employee.py
@@ -17,23 +17,11 @@ class HrEmployee(models.Model):
     type_id = fields.Many2one('hr.type.employee', 'Tipo de Empleado')
     formated_vat = fields.Char(translate=True, string='Printable VAT', store=True, help='Show formatted vat')
 
-    @api.model
-    def _get_computed_name(self, last_name, firstname, last_name2=None, middle_name=None):
-        names = []
-        if firstname:
-            names.append(firstname)
-        if middle_name:
-            names.append(middle_name)
-        if last_name:
-            names.append(last_name)
-        if last_name2:
-            names.append(last_name2)
-        return " ".join(names)
-
-    @api.onchange('firstname', 'mothers_name', 'middle_name', 'last_name')
-    def get_name(self):
-        if self.firstname and self.last_name:
-            self.name = self._get_computed_name(self.last_name, self.firstname, self.mothers_name, self.middle_name)
+    @api.onchange('firstname', 'last_name', 'mothers_name')
+    def _onchange_con_name(self):
+        if self.firstname and self.last_name and self.mothers_name:
+            self.name = ' '.join([self.firstname, self.middle_name or '',
+                                  self.last_name, self.mothers_name])
 
     @api.onchange('identification_id')
     def onchange_document(self):
