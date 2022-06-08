c24ce47ea48f904ce870cf97424faf0cd1e48d26
Manuel Vinent Guilarte <mjavint@gmail.com>
2022-06-08 16:45:46 -0400

    [UPG] Corregida la genereacion de la nomina

--- l10n_cl_hr/__manifest__.py
+++ l10n_cl_hr/__manifest__.py
@@ -72,7 +72,7 @@ Chilean Payroll & Human Resources.
         'data/l10n_cl_hr_mutual.xml',
         'data/l10n_cl_hr_apv.xml',
         'data/hr_type_employee.xml',
-        # 'data/resource_calendar_attendance.xml',
+        'data/resource_calendar_attendance.xml',
         'data/hr_holidays_status.xml',
         'data/hr_contract_type.xml',
         'data/l10n_cl_hr_ccaf.xml',
